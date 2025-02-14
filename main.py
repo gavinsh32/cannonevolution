e45683f5a6713a462d079e024a3ae7cf2c4694f9
gavinsh32 <gavinhaynes32@gmail.com>
2025-02-14 15:28:31 -0800

    Continued working on main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import simulator
 import population
 import cannon
 
+# Simulation settings
 width = 100
 height = 100
 targetx = 80
@@ -17,30 +18,46 @@ sim = simulator.Simulator()
 sim.initBounds(width, height)
 sim.initTarget(targetx, targety, width)
 
-pop = population.Population(n=psize)
+runs = []
 
-for epoch in range(0, 30):
+epochindex = [i for i in range(0, 30)]
+runindex = [i for i in range(0, 5)]
 
-    print('Generation', epoch, 'population size', pop.size())
+for run in runindex:
 
-    # Fire cannons, get a list of how close they came
-    dists = sim.fire(pop)
+    generations = []
+    pop = population.Population(n=psize)
 
-    # Select fit individuals based on the closest each came to the target
-    fit = sim.select(pop, dists, 20)
+    for epoch in epochindex:
 
-    print('Success:', int(fit.size() / pop.size() * 100), 'percent')
+        # Fire cannons, get a list of how close they came
+        dists = sim.fire(pop)       
+        # Select fit individuals based on the closest each came to the target
+        fit = sim.select(pop, dists, 0)        
 
-    # Mutate children
-    fit.mutateTilt(10, 3)
-    fit.mutatePower(0, 0)
+        # Record success
+        generations.append(fit.size() / pop.size())
 
-    fit.cull(70)
+        # Mutate children
+        fit.mutateTilt(0, 5)
+        fit.mutatePower(0, 0)  
 
-    # Cull initial population
-    pop.cull(10)
+        fit.cull(70)       
+        pop.cull(10)        
 
-    # Add children to population
-    pop.join(fit)
+        # Add children to population
+        pop.join(fit)      
 
-    # Repeat for each generation
+        # Repeat for each generation
+
+    runs.append(generations)
+
+# Calculate average success at each generation in every run
+gen_avgs = []
+for gen in epochindex:
+    avg = 0
+    for run in runindex:
+        avg += runs[run][gen]
+    print('Generation', gen, 'avg:', avg / len(runindex))
+
+data = {'generation': epochindex, 'average success': gen_avgs}
